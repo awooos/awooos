1d6293b13042631d05b2b39a888eab4fe0c8f180
Ellen Marie Dash <me@duckie.co>
2017-10-12 14:47:00 -0400

    [stddef.h] Add some notes; add NULL; add optimism.

--- include/stddef.h
+++ include/stddef.h
@@ -1,10 +1,24 @@
 #ifndef STDDEF_H
 #define STDDEF_H
 
+/* References
+ * =========
+ *
+ * http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1548.pdf
+ * https://sourceforge.net/p/predef/wiki/Standards/
+ *
+ */
+
+
+// I can dream.
+//#define __STDC_VERSION__ 201112L // C11
+
+#define NULL ((void*)0)
+
 #ifdef __x86_64
     // 8 bytes.
     typedef unsigned long int size_t;
-#else
+#elif __i386
     // 4 bytes.
     typedef unsigned int size_t;
 #endif
