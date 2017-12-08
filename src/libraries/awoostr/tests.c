212fc510653c1b548feeff78df5b9a713037584a
Ellen Marie Dash <me@duckie.co>
2017-12-08 02:02:02 -0500

    ?!

--- src/libraries/awoostr/tests.c
+++ src/libraries/awoostr/tests.c
@@ -1,8 +1,7 @@
 #include <ktest.h>
 #include <stdint.h>
+#include <awoostr.h>
 #include <awoo/tests.h>
-
-extern size_t _decimal_places_in_uint(uint64_t n, size_t radix);
 
 TestResult *test_awoostr_assertions()
 {
@@ -14,64 +13,24 @@ TestResult *test_awoostr_assertions()
     TEST_ASSERT(_decimal_places_in_uint(20, 10) == 2);
     TEST_ASSERT(_decimal_places_in_uint(21, 10) == 2);
 
-    /*if (uint64_mod(10, 10) == 0);
-        kprint("10 % 10 == 0\n");
-    } else {
-        kprint("10 % 10 != 0?\n");
-    }
-    if (uint64_mod(13, 10) == 3);
-        kprint("13 % 10 == 3\n");
-    } else {
-        kprint("13 % 10 != 3?\n");
-    }
-    if (uint64_mod(53, 12) == 5);
-        kprint("53 % 12 == 5\n");
-    } else {
-        kprint("53 % 12 != 5?\n");
-    }
-    if (uint64_mod(53, 13) == 1);
-        kprint("53 % 13 == 1\n");
-    } else {
-        kprint("53 % 13 != 1?\n");
-    }
-    if (uint64_mod(3, 10) == 3);
-        kprint(" 3 % 10 == 3\n");
-    } else {
-        kprint(" 3 % 10 != 3?\n");
-    }
-    if (uint64_mod(10, 3) == 1);
-        kprint("10 %  3 == 1\n");
-    } else {
-        kprint("10 %  3 != 1?\n");
-    }*/
-/*
-    kprint("should be 10:   ");
-    kprint(uint64_to_str_radix(10, 10));
-    kprint("\n");
-    kprint("should be 10:   ");
-    kprint(uint64_to_str_radix(10, 10));
-    kprint("\n");
+    // TODO: uint64_div() tests.
 
-    kprint("\n");
-    kprint("should be 0:    ");
-    kprint(str(0));
-    kprint("\n");
-    kprint("should be 1:    ");
-    kprint(str(1));
-    kprint("\n");
-    kprint("should be 2:    ");
-    kprint(str(2));
-    kprint("\n");
-    kprint("should be 10:   ");
-    kprint(str(10));
-    kprint("\n");
-    kprint("should be 123:  ");
-    kprint(str(123));
-    kprint("\n");
-    kprint("should be 9001: ");
-    kprint(str(9001));
-    kprint("\n");
-*/
+    TEST_ASSERT(uint64_mod(10, 10) == 0);
+    TEST_ASSERT(uint64_mod(13, 10) == 3);
+    TEST_ASSERT(uint64_mod(53, 12) == 5);
+    TEST_ASSERT(uint64_mod(53, 13) == 1);
+    TEST_ASSERT(uint64_mod(3, 10) == 3);
+    TEST_ASSERT(uint64_mod(10, 3) == 1);
+
+    TEST_ASSERT(strcmp(uint64_to_str_radix(10, 10), "10") == 0);
+    TEST_ASSERT(strcmp(uint64_to_str_radix(10, 10), "10") == 0);
+
+    TEST_ASSERT(strcmp(str(0), "0") == 0);
+    TEST_ASSERT(strcmp(str(1), "1") == 0);
+    TEST_ASSERT(strcmp(str(2), "2") == 0);
+    TEST_ASSERT(strcmp(str(123), "123") == 0);
+    TEST_ASSERT(strcmp(str(9001), "123") == 0);
+
     TEST_ASSERTIONS_RETURN();
 }
 
