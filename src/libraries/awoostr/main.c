b9120be681bed5ed81dd9f876cf2173e88708e69
Ellen Marie Dash <me@duckie.co>
2017-12-17 15:47:16 -0500

    fix uint64_to_str_radix()!

--- src/libraries/awoostr/main.c
+++ src/libraries/awoostr/main.c
@@ -50,12 +50,27 @@ size_t _decimal_places_in_uint(uint64_t n, size_t radix)
     return decimal_places;
 }
 
+extern void kprint(const char*);
+char uint8_to_char(uint8_t value)
+{
+    if (value < 10) { // 0-9
+        return '0' + (char)value;
+    } else if (value >= 10 && value < 36) { // A-Z
+        return 'a' + (char)value - 10;
+    } else {
+        // Not a representable character.
+        // Good luck have fun.
+        return '?';
+    }
+}
+
 // Converts a +uint64_t+ to a string, using the base specified in +radix+.
 //
 // ASSUMPTION: radix is small enough that all values can fit in a char.
 char *uint64_to_str_radix(uint64_t n, size_t radix)
 {
     char *result;
+    uint8_t value;
     uint64_t tmp = n;
 
     size_t decimal_places = decimal_places_in(n, radix);
@@ -64,7 +79,8 @@ char *uint64_to_str_radix(uint64_t n, size_t radix)
     memset(result, 0, decimal_places);
 
     for (size_t idx = 0; idx < decimal_places; idx++) {
-        result[idx] = '0' + (char)uint64_mod(tmp, radix);
+        value = (uint8_t)uint64_mod(tmp, radix);
+        result[idx] = uint8_to_char(value);
         tmp = uint64_div(tmp, radix);
     }
 
