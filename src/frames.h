b6ecdd52dd5b3be0079fec3337667c51cf63fee6
Ellen Marie Dash <me@duckie.co>
2017-12-26 10:00:23 -0500

    fix things.

--- src/frames.h
+++ src/frames.h
@@ -1,8 +1,8 @@
 #ifndef HAL_MM_FRAMES_H
 #define HAL_MM_FRAMES_H
 
-#include <kernel/hal/mm/placement_allocator.h>
-#include <kernel/hal/mm/paging.h>
+#include "placement_allocator.h"
+#include "paging.h"
 
 void set_frame(unsigned int addr);
 void clear_frame(unsigned int addr);
@@ -10,6 +10,6 @@ unsigned int test_frame(unsigned int addr);
 unsigned int first_frame();
 int alloc_frame(PageTableEntry *page, int is_kernel, int is_writable);
 void free_frame(PageTableEntry *page);
-void InitMMFrames();
+void dmm_frames_init();
 
 #endif /* HAL_MM_FRAMES_H */
