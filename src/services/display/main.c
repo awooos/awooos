38103d364386ab4d1320f8f36942a9eb7840fe86
Ellen Marie Dash <me@duckie.co>
2017-12-25 22:43:15 -0500

    add tiny display service so i can actually test multitasking/scheduler stuff.

---
+++ src/services/display/main.c
@@ -0,0 +1,13 @@
+// Prints '?!?!?!' to the screen.
+
+int main(int argc, char **argv)
+{
+    char *video = (char*)0xB8000;
+
+    for (int i = 0; i < 12; i += 2) {
+        video[i] = (i % 4 == 0) ? '!' : '?';
+        video[i + 1] = 0x07;
+    }
+
+    return 0;
+}
