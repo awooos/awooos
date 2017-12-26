e8685c30ca37852d3be4567dceb55f62d400c268
Ellen Marie Dash <me@duckie.co>
2017-12-26 10:43:47 -0500

    ?????

--- make/i386.mk
+++ make/i386.mk
@@ -8,4 +8,6 @@ override ASFLAGS += -felf32
 # i386-specific external libraries.
 override CFLAGS += -I src/libraries/dux-mm-i386/include
 
+KERNEL_EXE_LIBRARIES_APPEND := -l :dux-mm-i386.a
+
 QEMU ?= qemu-system-i386
