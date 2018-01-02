142cec1a103b96a6792eac7bbb1c6264c664273f
Ellen Marie Dash <me@duckie.co>
2018-01-01 23:16:44 -0500

    clean up kernel.h

--- include/kernel.h
+++ include/kernel.h
@@ -1,7 +1,7 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-#include <stdlib.h>
+#include <stddef.h>
 #include <stdbool.h>
 #include <eventually.h>
 #include <awoo/modifiers.h>
@@ -10,9 +10,7 @@ void hal_register_events();
 void tests_register_events();
 void greeter_register_events();
 
-void hal_test_shutdown(bool success);
-
-#define kprint(STRING) eventually_event_trigger_immediate("kernel print", (char*)STRING, 0);
+#define kprint(STRING) eventually_event_trigger_immediate("kernel print", (char*)STRING, 0)
 
 noreturn _panic(const char *message, const char *function,
                 const char* filename, size_t line, bool automated);
