fc1976467686dd15ba6753dd439e9384aa66e883
Ellen Marie Dash <me@duckie.co>
2018-01-03 15:24:36 -0500

    DESTROY EVERYTHING AND TRY AGAIN. HASHTAG YOLO HASHTAG YOLOGRAMMING HASHTAG FUCKIT

--- include/dmm.h
+++ include/dmm.h
@@ -1,10 +1,17 @@
-#ifndef DUX_DMM_H
-#define DUX_DMM_H
+#ifndef DMM_H
+#define DMM_H
 
 #include <stddef.h>
 
-void dmm_init(size_t _kernel_end, size_t _end_memory);
-void *kmalloc(size_t nbytes);
-void kfree(void *ap);
+void dmm_add_memory_region(void *start, size_t length);
+void *dmm_malloc(size_t size);
+void dmm_free(void *ptr);
+
+// If the DMM_INTRUSIVE macro is defined, alias malloc() and free()
+// to the dmm ones.
+#ifdef DMM_INTRUSIVE
+#define malloc dmm_malloc
+#define free dmm_free
+#endif
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
 #include <dmm.h>
 #include <stddef.h>
-#include "placement_allocator.h"
-#include "frames.h"
-#include "paging.h"
 
-void dmm_init(size_t last_used_address, size_t end_memory)
+void dmm_add_memory_region(void *start, size_t length)
 {
-    dmm_frames_init(end_memory);
-    dmm_paging_init();
-    dmm_placement_allocator_init(last_used_address);
+    //
+}
+
+void *dmm_malloc(size_t size)
+{
+    //
+}
+
+void dmm_free(void *ptr)
+{
+    //
 }
