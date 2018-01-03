e57eac17680fa1fba81d7ee61a61cf150c5dbc96
Ellen Marie Dash <me@duckie.co>
2018-01-02 20:36:45 -0500

    yay fixed nightly-build.sh script.

--- bin/nightly-build.sh
+++ bin/nightly-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -v "$(pwd):/tmp/work" -w /tmp/work whaledo/os-development-x86 BUILD_TYPE=nightly NAME_SUFFIX="-$(date +'%Y-%m-%d') iso
+# Hard-code UID to 1000 so Jenkins is happy.
+docker run -v "$(pwd):/tmp/work" -w /tmp/work -u 1000 whaledo/os-development-x86 make BUILD_TYPE=nightly NAME_SUFFIX="-$(date +'%Y-%m-%d')" iso
