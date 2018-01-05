758d90d3b58b0c89f82487ddc78fda29653dd103
Ellen Marie Dash <me@duckie.co>
2018-01-05 13:22:12 -0500

    add string.c + string.h.

---
+++ include/string.h
@@ -0,0 +1,28 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stddef.h>
+
+void *memcpy(void *dest, const void *src, size_t n);
+
+extern void *memmove_tmp(void *dest, const char *src, size_t n, void *tmp);
+//extern void *memmove(void *dest, const char *src, size_t n);
+extern void *memset(void *s, int c, size_t n);
+
+extern char *strcpy(char *dest, const char *src);
+extern char *strncpy(char *dest, const char *src, size_t n);
+
+// extern char *strcat(char *dest, const char *src);
+// extern char *strncat(char *dest, const char *src, size_t n);
+
+extern int strcmp(const char *s1, const char *s2);
+extern int strncmp(const char *s1, const char *s2, size_t n);
+
+// extern char *strchr(const char *s, int c);
+// extern char *strrchr(const char *s, int c);
+
+extern size_t strlen(const char *str);
+
+char *strrev(char *str);
+
+#endif
---
+++ src/tests.c
@@ -0,0 +1,12 @@
+/*
+#include <ktest.h>
+#include <stdint.h>
+#include <awoostr.h>
+#include <awoo/tests.h>
+#include <stdlib.h>
+*/
+
+void add_libc_tests()
+{
+    // TODO: Tests. (All of the tests originally here were moved to dmm.)
+}
