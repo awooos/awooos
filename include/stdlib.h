9cda83e0b36435362786bbed3a39585c70b25bc4
Ellen Marie Dash <me@duckie.co>
2018-01-05 13:59:57 -0500

    add stdlib.h.

---
+++ include/stdlib.h
@@ -0,0 +1,11 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+
+#include <stddef.h>
+
+void *malloc(size_t size);
+void free(void *ptr);
+void *calloc(size_t nmemb, size_t size);
+void *realloc(void *ptr, size_t size);
+
+#endif
