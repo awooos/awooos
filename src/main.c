c0ecc73d56b4323897c8ec640227c0c8d1fa0ab1
Ellen Marie Dash <me@duckie.co>
2018-01-05 15:21:20 -0500

    use passed-in flail_print() function pointer, instead of assuming kprint() exists.

--- src/main.c
+++ src/main.c
@@ -2,17 +2,19 @@
 #include <stddef.h>
 #include <ali/str.h>
 
-// stack_dump() is an assembly stub (in libpanic-i386/main.asm), which calls
-// panic_stack_dump_hex() with the argument being the stack pointer (esp).
-extern void stack_dump();
+typedef void FlailPrintFn(const char*);
 
+// flail_stack_dump() is an assembly stub (in platform-i386/main.asm), which
+// call flail_stack_dump_hex() with the argument being the stack pointer (esp).
+extern void flail_stack_dump();
 static unsigned int in_panic = 0;
-
 char *info_str = NULL;
+FlailPrintFn *flail_print = NULL;
 
-void flail_init(char *info_str_)
+void flail_init(char *info_str_, FlailPrintFn *flail_print_)
 {
     info_str = info_str_;
+    flail_print = flail_print_;
 }
 
 void flail_stack_dump_hex(size_t *_stack)
@@ -22,11 +24,11 @@ void flail_stack_dump_hex(size_t *_stack)
     for (size_t original_stack = (size_t)stack;
             (size_t)stack < ((original_stack + 0x1000) & (size_t)(~(0x1000 - 1)));
             stack++) {
-        kprint("0x");
-        kprint(n_to_str_radix((size_t)stack, 16));
-        kprint(": 0x");
-        kprint(n_to_str_radix(*stack, 16));
-        kprint("\n");
+        flail_print("0x");
+        flail_print(n_to_str_radix((size_t)stack, 16));
+        flail_print(": 0x");
+        flail_print(n_to_str_radix(*stack, 16));
+        flail_print("\n");
 
         if (*stack == 0x0) {
             break;
@@ -44,28 +46,28 @@ void _flail_panic(const char *message, const char *function,
     if (in_panic == 0) {
         in_panic = 1;
 
-        kprint("!!! Kernel panic !!!\r\n\r\n");
-        kprint(info_str);
-        kprint("\r\n\r\n");
+        flail_print("!!! Kernel panic !!!\r\n\r\n");
+        flail_print(info_str);
+        flail_print("\r\n\r\n");
 
-        kprint(message);
-        kprint("\r\n\r\n");
+        flail_print(message);
+        flail_print("\r\n\r\n");
 
-        kprint("Stack dump:\r\n\r\n");
+        flail_print("Stack dump:\r\n\r\n");
 
         flail_stack_dump();
 
-        kprint("\r\n\r\n");
+        flail_print("\r\n\r\n");
 
-        kprint("Kernel panic: ");
-        kprint(message);
-        kprint("\r\n ");
-        kprint(filename);
-        kprint(":");
-        kprint(str(line));
-        kprint(" (");
-        kprint(function);
-        kprint(")\r\n");
+        flail_print("Kernel panic: ");
+        flail_print(message);
+        flail_print("\r\n ");
+        flail_print(filename);
+        flail_print(":");
+        flail_print(str(line));
+        flail_print(" (");
+        flail_print(function);
+        flail_print(")\r\n");
     }
 
     if (automated == 0) {
