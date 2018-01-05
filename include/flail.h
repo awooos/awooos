06c9e14a9f8385affa8b8d08dd726218ba45f96e
Ellen Marie Dash <me@duckie.co>
2018-01-05 15:50:18 -0500

    don't define panic(), just flail_panic().

--- include/flail.h
+++ include/flail.h
@@ -7,6 +7,5 @@ void _flail_panic(const char *message, const char *function,
                 const char* filename, size_t line, size_t automated);
 
 #define flail_panic(message) _flail_panic(message, __FUNCTION__, __FILE__, #__LINE__, 0)
-#define panic(message) flail_panic(message)
 
 #endif
