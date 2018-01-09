d75127266ed1b0cfcdf8c8f1663c3eb642f33254
Ellen Marie Dash <me@duckie.co>
2018-01-08 20:27:56 -0500

    ????

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,7 @@
-FROM 
+FROM alpine:edge
+
+RUN apk add --no-cache python3 git
+
+COPY bin/ /tmp/bin
+
+ENTRYPOINT ["/tmp/bin/tinker.py"]
--- bin/tinker.py
+++ bin/tinker.py
@@ -3,13 +3,24 @@
 import re
 import json
 import sys
+import subprocess.check_call as check_call
+from contextlib import contextmanager
 
-def header_source(repo):
-    return "#include <{}.h>".format(repo["name"])
+@contextmanager
+def chdir(newdir):
+    curdir = os.getcwd()
+    os.chdir(newdir)
+    try:
+        yield
+    finally:
+        os.chdir(curdir)
 
-def initializer_source(repo):
-    name = repo["name"]
-    args = ", ".join(repo["initializeArgs"])
+def header_source(component):
+    return "#include <{}.h>".format(component["name"])
+
+def initializer_source(component):
+    name = component["name"]
+    args = ", ".join(component["initializeArgs"])
 
     return "    {}_init({});".format(name, args)
 
@@ -21,10 +32,10 @@ def runner_source(repo):
     else:
         return "    {}_run();".format(name)
 
-def kernel_source(repos = []):
-    headers = map(header_source, repos)
-    initializers = map(initializer_source, repos)
-    runners = map(runner_source, repos)
+def kernel_source(components = []):
+    headers = map(header_source, components)
+    initializers = map(initializer_source, components)
+    runners = map(runner_source, components)
     runners = filter(lambda x: x != None, runners)
 
     kernel_source = "" + \
@@ -37,9 +48,96 @@ def kernel_source(repos = []):
 
     return kernel_source
 
-with open(sys.argv[1], "r") as f:
+def tinker_dir(folder = ""):
+    return os.path.normpath(os.path.join(os.getcwd(), "tinker", folder))
+
+# ASSUMPTION: A directory with an @ symbol in it won't break things.
+# ASSUMPTION: A directory with an @ symbol at the end won't break things.
+# ASSUMPTION: Nobody cares if they have a directory ending with @ in ./tinker/.
+def tinker_repo_dir(component_name, branch = ""):
+    return tinker_dir("{}@{}".format(component_name, branch))
+
+def ensure_tinker_root_exists():
+    tinker_dir_ = tinker_dir()
+    if not os.path.exists(tinker_dir_):
+        # If it doesn't exist, create it.
+        os.mkdir(tinker_dir_)
+    elif not os.path.isdir(tinker_dir_):
+        raise NotADirectoryError("expected {} to be a directory.".format(tinker_dir_))
+
+def git_clone(dependency):
+    origin = dependency["origin"]
+
+    if "#" in origin:
+        repo, branch = origin.split("#")
+        branch_args = ["--branch", branch]
+    else:
+        repo = origin
+        branch_args = [""]
+
+    destination = tinker_repo_dir(dependency)
+    with chdir(tinker_dir()):
+        check_call(["git", "clone", *branch_args, "--", repo, destination])
+
+def git_pull(dependency):
+    with chdir(tinker_dir_for(dependency)):
+        check_call(["git", "pull"])
+
+def clone_or_pull(dependency):
+    ensure_tinker_root_exists()
+
+    if not os.isdir(tinker_repo_dir(dependency)):
+        git_clone(dependency)
+    else:
+        git_pull(dependency)
+
+# ASSUMPTIONS: Dependencies are either git repos or local files.
+def fetch_dependency(dependency):
+    origin = dependency["origin"]
+
+    if origin[0] == "/" or origin[0] == ".":
+        root = origin
+    else:
+        root = clone_or_pull(origin)
+
+    dependency["root"] = root
+
+    return dependency
+
+def generate_kernel(dependencies):
+    dependencies = map(fetch_dependency, dependency)
+    component_names = map(lambda x: x["name"], dependencies)
+
+    source = kernel_source(component_names)
+
+    return {
+            "source": source,
+            "dependencies": dependencies,
+            }
+
+if len(sys.argv) != 3 or sys.argv[1] == "--help" or sys.argv[1] == "-h":
+    print("Usage: tinker.py tinker.json output_file.c".format(sys.argv[0]))
+    sys.exit(1)
+
+config_file = sys.argv[1]
+output_file = sys.argv[2]
+
+with open(config_file, "r") as f:
     config = json.load(f)
 
-results = kernel_source(config)
+kernel = generate_kernel(config)
 
-print(results)
+with open(output_file, "w") as f:
+    f.write(kernel["source"])
+
+print("Generated kernel with:")
+for dep in kernel["dependencies"]:
+    print(" - {} ({})".format(dep["name"], dep["origin"]))
+
+print("")
+print("")
+
+print("Output file: {}".format(output_file))
+
+
+
