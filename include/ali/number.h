228e0640958c9b7f31749d98eecd7bbf508bf912
Ellen Marie Dash <me@duckie.co>
2018-01-28 18:19:05 -0500

    lol i'm bad at this.

--- include/ali/number.h
+++ include/ali/number.h
@@ -46,8 +46,8 @@ uint64_t uint64_mod(uint64_t a, uint64_t b);
 char *uint64_to_str_radix(uint64_t n, size_t radix);
 char *int64_to_str_radix(int64_t n, size_t radix);
 
-#define uint_to_str_radix(n, radix) uint64_to_str_radix((uint64)(n), radix)
-#define  int_to_str_radix(n, radix)  int64_to_str_radix((uint64)(n), radix)
+#define uint_to_str_radix(n, radix) uint64_to_str_radix((uint64_t)(n), radix)
+#define  int_to_str_radix(n, radix)  int64_to_str_radix((uint64_t)(n), radix)
 
 #define uint_to_str(n) uint_to_str_radix((n), 10)
 #define  int_to_str(n)  int_to_str_radix((n), 10)
