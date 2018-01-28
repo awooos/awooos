22ff868b5f79ba5166548bfe2eaaf7627493327a
Ellen Marie Dash <me@duckie.co>
2018-01-28 17:42:04 -0500

    Fix number_test.c.

--- src/number_test.c
+++ src/number_test.c
@@ -19,61 +19,61 @@ size_t test_ali_signedness_assertions()
 {
     TEST_HAS_ASSERTIONS();
 
-    // Unsigned +1.
+    // Unsigned +1. Should return 1.
     TEST_ASSERT(is_unsigned((unsigned char)1) == 1);
     TEST_ASSERT(is_unsigned((unsigned short)1) == 1);
     TEST_ASSERT(is_unsigned((unsigned int)1) == 1);
     TEST_ASSERT(is_unsigned((unsigned long)1) == 1);
     TEST_ASSERT(is_unsigned((unsigned long long)1) == 1);
 
-    // Unsigned -1.
+    // Unsigned -1. Should return 1.
     TEST_ASSERT(is_unsigned((unsigned char)-1) == 1);
     TEST_ASSERT(is_unsigned((unsigned short)-1) == 1);
     TEST_ASSERT(is_unsigned((unsigned int)-1) == 1);
     TEST_ASSERT(is_unsigned((unsigned long)-1) == 1);
     TEST_ASSERT(is_unsigned((unsigned long long)-1) == 1);
 
-    // Signed +1.
-    TEST_ASSERT(is_unsigned((signed char)1) == 1);
-    TEST_ASSERT(is_unsigned((signed short)1) == 1);
-    TEST_ASSERT(is_unsigned((signed int)1) == 1);
-    TEST_ASSERT(is_unsigned((signed long)1) == 1);
-    TEST_ASSERT(is_unsigned((signed long long)1) == 1);
+    // Signed +1. Should return 0.
+    TEST_ASSERT(is_unsigned((signed char)1) == 0);
+    TEST_ASSERT(is_unsigned((signed short)1) == 0);
+    TEST_ASSERT(is_unsigned((signed int)1) == 0);
+    TEST_ASSERT(is_unsigned((signed long)1) == 0);
+    TEST_ASSERT(is_unsigned((signed long long)1) == 0);
 
-    // Signed -1.
-    TEST_ASSERT(is_unsigned((signed char)-1) == 1);
-    TEST_ASSERT(is_unsigned((signed short)-1) == 1);
-    TEST_ASSERT(is_unsigned((signed int)-1) == 1);
-    TEST_ASSERT(is_unsigned((signed long)-1) == 1);
-    TEST_ASSERT(is_unsigned((signed long long)-1) == 1);
+    // Signed -1. Should return 0.
+    TEST_ASSERT(is_unsigned((signed char)-1) == 0);
+    TEST_ASSERT(is_unsigned((signed short)-1) == 0);
+    TEST_ASSERT(is_unsigned((signed int)-1) == 0);
+    TEST_ASSERT(is_unsigned((signed long)-1) == 0);
+    TEST_ASSERT(is_unsigned((signed long long)-1) == 0);
 
-    // Unsigned +255.
+    // Unsigned +255. Should return 1.
     TEST_ASSERT(is_unsigned((unsigned char)255) == 1);
     TEST_ASSERT(is_unsigned((unsigned short)255) == 1);
     TEST_ASSERT(is_unsigned((unsigned int)255) == 1);
     TEST_ASSERT(is_unsigned((unsigned long)255) == 1);
     TEST_ASSERT(is_unsigned((unsigned long long)255) == 1);
 
-    // Unsigned -255.
+    // Unsigned -255. Should return 1.
     TEST_ASSERT(is_unsigned((unsigned char)-255) == 1);
     TEST_ASSERT(is_unsigned((unsigned short)-255) == 1);
     TEST_ASSERT(is_unsigned((unsigned int)-255) == 1);
     TEST_ASSERT(is_unsigned((unsigned long)-255) == 1);
     TEST_ASSERT(is_unsigned((unsigned long long)-255) == 1);
 
-    // Signed +255.
-    TEST_ASSERT(is_unsigned((signed char)255) == 1);
-    TEST_ASSERT(is_unsigned((signed short)255) == 1);
-    TEST_ASSERT(is_unsigned((signed int)255) == 1);
-    TEST_ASSERT(is_unsigned((signed long)255) == 1);
-    TEST_ASSERT(is_unsigned((signed long long)255) == 1);
+    // Signed +255. Should return 0.
+    TEST_ASSERT(is_unsigned((signed char)255) == 0);
+    TEST_ASSERT(is_unsigned((signed short)255) == 0);
+    TEST_ASSERT(is_unsigned((signed int)255) == 0);
+    TEST_ASSERT(is_unsigned((signed long)255) == 0);
+    TEST_ASSERT(is_unsigned((signed long long)255) == 0);
 
-    // Signed -255.
-    TEST_ASSERT(is_unsigned((signed char)-255) == 1);
-    TEST_ASSERT(is_unsigned((signed short)-255) == 1);
-    TEST_ASSERT(is_unsigned((signed int)-255) == 1);
-    TEST_ASSERT(is_unsigned((signed long)-255) == 1);
-    TEST_ASSERT(is_unsigned((signed long long)-255) == 1);
+    // Signed -255. Should return 0.
+    TEST_ASSERT(is_unsigned((signed char)-255) == 0);
+    TEST_ASSERT(is_unsigned((signed short)-255) == 0);
+    TEST_ASSERT(is_unsigned((signed int)-255) == 0);
+    TEST_ASSERT(is_unsigned((signed long)-255) == 0);
+    TEST_ASSERT(is_unsigned((signed long long)-255) == 0);
 
     TEST_ASSERTIONS_RETURN();
 }
