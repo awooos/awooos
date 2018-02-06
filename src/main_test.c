9f9dde4667b927d0f4e8eb0e53e5f19265fc8404
Alice Jenkinson <0x52a100ff@gmail.com>
2018-02-07 09:08:32 +1300

    Add allocate-the-universe test

--- src/main_test.c
+++ src/main_test.c
@@ -37,3 +37,55 @@ size_t test_dmm_free_sets_header()
 
     TEST_ASSERTIONS_RETURN();
 }
+
+size_t test_dmm_allocate_the_universe()
+{
+    DMM_MallocHeader *header;
+
+    // Allocate everything
+    while (1) {
+        void *region = dmm_malloc(ALLOCATE_THE_UNIVERSE_CHUNK_SIZE);
+        if (region == NULL) {
+            break;
+        }
+
+        header = ((DMM_MallocHeader*)region) - 1;
+        header->flags = DMM_HEADER_FLAG_TEST;
+    }
+
+    // Check that all the allocations with the test flag set are of the size
+    // we expect them to be
+    header = dmm_global_instance;
+    while (1) {
+        if (header == NULL) {
+            break;
+        }
+
+        if ((header->flags & DMM_HEADER_FLAG_TEST) == 1) {
+            if (header->size != (ALLOCATE_THE_UNIVERSE_CHUNK_SIZE - sizeof(DMM_MallocHeader))) {
+                TEST_RETURN(TEST_FAILURE, "An allocated test chunk was not of the expected size.");
+            }
+        }
+
+        header = header->next;
+    }
+
+    // Free all the test allocations
+    header = dmm_global_instance;
+    while (1) {
+        if (header == NULL) {
+            break;
+        }
+
+        // Save the value of header->next in case the header disappears when we
+        // free() the region (when DMM actually merges free regions)
+        DMM_MallocHeader *next = header->next;
+        if ((header->flags & DMM_HEADER_FLAG_TEST) == 1) {
+            dmm_free(header->data);
+        }
+
+        header = next;
+    }
+
+    TEST_RETURN(TEST_SUCCESS, "Allocating the universe test passed.");
+}
--- src/main_test.h
+++ src/main_test.h
@@ -5,7 +5,10 @@
 #include <dmm.h>
 #include "main.h"
 
+#define ALLOCATE_THE_UNIVERSE_CHUNK_SIZE (1024 * 1024) // 1M
+
 size_t test_dmm_malloc();
 size_t test_dmm_free_sets_header();
+size_t test_dmm_allocate_the_universe();
 
 #endif
