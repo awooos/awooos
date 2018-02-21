93a22c108dfe25af916673ed6829c3c3f1515c83
Ellen Marie Dash <me@duckie.co>
2018-02-20 22:20:54 -0500

    Add include/assert.h.

---
+++ include/assert.h
@@ -0,0 +1,27 @@
+// POSIX SPEC VIOLATION: We always define assert(), even though the POSIX
+//                       standard dictates that it should be redefined
+//                       to either a no-op or the normal implementation
+//                       _every time assert.h is included_ based on the
+//                       existence of the NDEBUG macro.
+//
+//                       Details: http://en.cppreference.com/w/c/error/assert
+//
+//                       I decided it was not worth the effort, given the
+//                       complexity of doing so and this quote from Linux's
+//                       `man assert` (aka the Linux Programmer's Manual):
+//                          "It is not recommended to define NDEBUG if
+//                           using assert()"
+//
+//                       If you disagree with my decision, please _do not
+//                       hesitate_ to open an issue on GitHub!
+//                          https://github.com/awooos/ali/issues
+//
+//                       - @duckinator, 2018-02-20
+
+#ifndef ASSERT_H
+#define ASSERT_H
+
+#define assert(expr) ((expr) || __ali_assert_fail(#expr, __FILE__, __LINE__, __func__))
+#define static_assert _Static_assert
+
+#endif
