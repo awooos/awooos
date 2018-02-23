a6f459de281b64e052e98c15bf023b710d9bb536
Ellen Marie Dash <me@duckie.co>
2018-02-22 21:37:36 -0500

    Have separate _flail_panic() and _flail_print_panic() instead of an argument to change the behavior.

--- src/main_test.c
+++ src/main_test.c
@@ -7,14 +7,11 @@
 // On Travis CI, this will result in a build error after 10 minutes of
 // printing no output.
 //
-// This isn't the most robust test, but it's good enough to ensure that
-// panic() is behaving at least close to how it's expected to.
+// This isn't the most robust test, but it's better than nothing.
 //
-// NOTE: We use _flail_panic() instead of the usual flail_panic(), so we can pass
-// "true", which indicates that it's an automated panic and the system
-// should do a hard shutdown immediately after printing the panic
-// information. (As opposed to the usual endless loop.)
+// NOTE: We use _flail_print_panic() instead of the usual flail_panic() so
+//       we can continue running after the fake panic.
 void test_flail_intentional_panic()
 {
-    _flail_panic("This kernel panic is intentional.", __FUNCTION__, __FILE__, __LINE__, 1);
+    _flail_print_panic("This kernel panic is intentional.", __FUNCTION__, __FILE__, __LINE__);
 }
