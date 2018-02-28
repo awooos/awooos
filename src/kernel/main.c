a4a3ce27041cc192c66891788ce0f482c837aefc
Ellen Marie Dash <me@duckie.co>
2018-02-28 11:02:18 -0500

    [WIP] pull ktest out into tinker.

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -8,6 +8,7 @@ noreturn void kernel_main()
     greeter_register_events();
     tests_register_events();
     hal_register_events();
+    ktest_init(&kprint);
 
     event_trigger("HAL init", NULL, 0);
     event_trigger("greeter display", NULL, 0);
