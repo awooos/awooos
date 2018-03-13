46b8fad0047d4a4d84d5555d30ba24aa86350a41
Deepesh Choudhary <34906643+Deepesh-Choudhary@users.noreply.github.com>
2018-03-13 15:57:30 +0530

    Fix output in main.c
    The comment says "?!?!?!" is printed, but the code printed "!?!?!?".

--- src/services/display/main.c
+++ src/services/display/main.c
@@ -6,7 +6,7 @@ int main(UNUSED int argc, UNUSED char **argv)
     char *video = (char*)0xB8000;
 
     for (int i = 0; i < 12; i += 2) {
-        video[i] = (i % 4 == 0) ? '!' : '?';
+        video[i] = (i % 4 == 0) ? '?' : '!';
         video[i + 1] = 0x07;
     }
 
