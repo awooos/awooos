6b164e2e4d872b69b882b1b2de7805d1b9ef829d
Ellen Marie Dash <me@duckie.co>
2018-05-10 03:31:41 -0400

    aksjdfasfdsa

--- src/str/shellsplit_test.c
+++ src/str/shellsplit_test.c
@@ -10,7 +10,7 @@ size_t test_ali_str_shellsplit()
     TEST_HAS_ASSERTIONS();
 
     const char *test_str = "test \"test test\" 'test test' test\\ test";
-    ShellSplitResults *test_output = shellsplit((char*)test_str);
+    ShellSplitResult *test_output = shellsplit((char*)test_str);
     TEST_ASSERT(test_output->num_pieces == 4);
     TEST_ASSERT(strcmp(test_output->pieces[0], "test") == 0);
     TEST_ASSERT(strcmp(test_output->pieces[1], "test test") == 0);
