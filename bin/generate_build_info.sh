e8938460ec9319ae73651e8b099cf9fd7752d96e
Ellen Marie Dash <me@duckie.co>
2018-05-25 10:08:08 -0400

    Clean up generate_build_info.sh + awoo/build_info.h.

--- bin/generate_build_info.sh
+++ bin/generate_build_info.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-TARGET_NORMAL="$2"
+TARGET_PRETTY="$2"
 TARGET=$(echo "$2" | tr [a-z] [A-Z])
 
 UPPER_CASE_BUILD_TYPE=$(echo "$1" | tr "[a-z]" "[A-Z]")
 LOWER_CASE_BUILD_TYPE=$(echo "$1" | tr "[A-Z]" "[a-z]")
 
-VERSION=$(git describe --tags HEAD --always)-${LOWER_CASE_BUILD_TYPE}
+SCM_REV=$(git describe --tags HEAD --always)
 
 echo  "#ifndef AWOO_BUILD_INFO_H"
 echo  "#define AWOO_BUILD_INFO_H"
-echo  "#define AWOO_SCM_REV \"$(git show | head -n1 | cut -c8-)\""
+echo  "#define AWOO_SCM_REV \"${SCM_REV}\""
 echo  "#define AWOO_BUILD_TYPE \"${UPPER_CASE_BUILD_TYPE}\""
-echo  "#define AWOO_VERSION \"${VERSION}\""
-echo  "#define AWOO_TARGET_PRETTY \"${TARGET_NORMAL}\""
+echo  "#define AWOO_PRETTY_BUILD_TYPE \"${LOWER_CASE_BUILD_TYPE}\""
 echo  "#define AWOO_TARGET \"${TARGET}\""
+echo  "#define AWOO_TARGET_PRETTY \"${TARGET_PRETTY}\""
 echo  "#define AWOO_${TARGET}"
 echo  "#define AWOO_RELEASE_BUILD   0"
 echo  "#define AWOO_DEBUG_BUILD     1"
