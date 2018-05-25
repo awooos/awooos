dbcc4dfb800b027fde861b7345e55c373b35b0d8
Ellen Marie Dash <me@duckie.co>
2018-05-25 10:20:33 -0400

    Remove unused things in awoo/build_info.h.

--- bin/generate_build_info.sh
+++ bin/generate_build_info.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-TARGET_PRETTY="$2"
-TARGET=$(echo "$2" | tr [a-z] [A-Z])
-
-BUILD_TYPE=$(echo "$1" | tr "[A-Z]" "[a-z]")
-
 SCM_REV=$(git describe --tags HEAD --always)
 
 if [ "${BUILD_TYPE}" == "test" ]; then
@@ -16,9 +11,7 @@ fi
 echo  "#ifndef AWOO_BUILD_INFO_H"
 echo  "#define AWOO_BUILD_INFO_H"
 echo  "#define AWOO_SCM_REV       \"${SCM_REV}\""
-echo  "#define AWOO_BUILD_TYPE    \"${BUILD_TYPE}\""
-echo  "#define AWOO_TARGET        \"${TARGET}\""
-echo  "#define AWOO_TARGET_PRETTY \"${TARGET_PRETTY}\""
-echo  "#define AWOO_${TARGET}     1"
+echo  "#define AWOO_BUILD_TYPE    \"$1\""
+echo  "#define AWOO_TARGET        \"$2\""
 echo  "#define AWOO_TEST_BUILD    ${TEST_BUILD}"
 echo  "#endif"
