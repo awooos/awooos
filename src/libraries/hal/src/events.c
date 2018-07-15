2540ea40148dd975a49225c28976f81c4b0d3d0b
Ellen Marie Dash <me@duckie.co>
2018-07-15 15:49:21 -0400

    move Flail initialization out of hal/*/events.c.

--- src/libraries/hal/src/events.c
+++ src/libraries/hal/src/events.c
@@ -1,13 +1,11 @@
 #include <ali/event.h>
 #include <hal.h>
 #include <dmm.h>
-#include "hal_flail.h"
 #include "panic.h"
 
 __attribute__((constructor))
 void hal_register_events()
 {
-    event_watch("HAL init",                &hal_flail_init);
     hal_register_platform_events();
     event_watch("register panic function", (void (*)(void*))hal_panic_init);
     event_watch("register panic function", (void (*)(void*))dmm_init);
