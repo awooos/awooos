2e9a4bd435a86e8f20dbfa123f005fbedfd3b327
Ellen Marie Dash <me@duckie.co>
2018-07-15 21:18:23 -0400

    reindent file; remove unneeded function declaration.

--- src/libraries/hal/platform-i386/idt.h
+++ src/libraries/hal/platform-i386/idt.h
@@ -5,16 +5,16 @@
 #include <stddef.h>
 
 typedef struct idt_pointer_s {
-	uint16_t size;
-	uint32_t offset __attribute__((packed));
+    uint16_t size;
+    uint32_t offset __attribute__((packed));
 } IdtPointer;
 
 typedef struct idt_entry_s {
-	uint16_t offset_lo;
-	uint16_t selector;
-	uint8_t zero;
-	uint8_t type;
-	uint16_t offset_hi;
+    uint16_t offset_lo;
+    uint16_t selector;
+    uint8_t zero;
+    uint8_t type;
+    uint16_t offset_hi;
 } IdtEntry;
 
 #define IDT_TASK 0x5
@@ -23,7 +23,6 @@ typedef struct idt_entry_s {
 #define IDT_TRAP16 0x7
 #define IDT_TRAP32 0xf
 
-void hal_idt_init();
 void hal_idt_register_exception(size_t n, uint32_t offset);
 
 #endif
