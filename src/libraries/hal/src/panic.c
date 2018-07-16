f050059b9085668b85899bb79686663a7263d23e
Ellen Marie Dash <me@duckie.co>
2018-07-15 21:08:25 -0400

    merge hal/src/events.c into hal/src/panic.c.

--- src/libraries/hal/src/panic.c
+++ src/libraries/hal/src/panic.c
@@ -1,9 +1,18 @@
-#include <stddef.h>
+#include <ali/event.h>
+#include <dmm.h>
 #include "panic.h"
+#include <stddef.h>
 
 Hal_PanicFn *_hal_panic = NULL;
 
 void hal_panic_init(Hal_PanicFn *panicfn)
 {
     _hal_panic = panicfn;
+}
+
+__attribute__((constructor))
+void hal_panic_register_events()
+{
+    event_watch("register panic function", (void (*)(void*))hal_panic_init);
+    event_watch("register panic function", (void (*)(void*))dmm_init);
 }
