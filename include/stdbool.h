541728358573812a867a8d2ee1b86e30ae90ff4d
Ellen Marie Dash <me@duckie.co>
2018-07-24 23:17:19 -0400

    Avoid using reserved identifiers without warning the compiler first.

--- include/stdbool.h
+++ include/stdbool.h
@@ -6,6 +6,12 @@
 #define true 1
 #define false 0
 #define bool _Bool
-#define __bool_true_false_are_defined 1
+
+// Disable warnings/errors for reserved ID macros, so we can define one.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+    // Define said macro
+    #define __bool_true_false_are_defined 1
+#pragma clang diagnostic pop
 
 #endif
