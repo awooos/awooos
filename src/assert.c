c58b6d03937fe1d539563a1099411ef57602b9e9
Ellen Marie Dash <me@duckie.co>
2018-07-24 23:31:40 -0400

    Very, very rough attempt at assert().

--- src/assert.c
+++ src/assert.c
@@ -3,5 +3,14 @@
 void __ali_assert_fail(const char *expr, const char *file,
                         unsigned int line, const char *func)
 {
-    // TODO: Implement me!
+    // TODO: Implement a significantly better version.
+    print("???: ");
+    print(file);
+    print(":");
+    print_number(line);
+    print(": ");
+    print(func);
+    print("Assertion `");
+    print(expr);
+    print("' failed.");
 }
