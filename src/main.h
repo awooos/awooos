bc342c80959d743ce529d683177c9b73a548ca82
Ellen Marie Dash <me@duckie.co>
2018-07-24 22:56:51 -0400

    Fix function prototypes.

--- src/main.h
+++ src/main.h
@@ -3,9 +3,9 @@
 
 // flail_stack_dump() is an assembly stub (in platform-i386/main.asm), which
 // call flail_stack_dump_hex() with the argument being the stack pointer (esp).
-extern void flail_stack_dump();
+extern void flail_stack_dump(void);
 
-void flail_wait_forever();
+void flail_wait_forever(void);
 
 void _flail_print_panic(const char *message, const char *function,
         const char *filename, size_t line);
