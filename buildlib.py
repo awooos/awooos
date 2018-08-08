f8f811c7feb6a51446aabac300ac105bcba3138e
Ellen Marie Dash <me@duckie.co>
2018-08-07 23:35:56 -0400

    ?????

--- buildlib.py
+++ buildlib.py
@@ -7,10 +7,12 @@ import sys
 def env(key, default=None):
     return os.environ.get(key, default)
 
+
 # Miscellaneous file suffix manipulation stuff.
 
 def is_filetype(suffix):
     return lambda x: x.endswith(suffix)
+
 
 def with_suffix(suffix):
     return lambda x: str(Path(x).with_suffix(suffix))
@@ -23,6 +25,7 @@ def recipe_expand(command):
                 for x in command]
     return cmd
 
+
 recipes = {}
 def recipe(target, match, deps, command):
     deps = set(deps)
@@ -32,6 +35,7 @@ def recipe(target, match, deps, command):
         command = recipe_expand(command)
     recipes[target] = (match, deps, command)
 
+
 def task(target, deps, command):
     recipe(target, None, deps, command)
 
@@ -39,16 +43,34 @@ def task(target, deps, command):
 
 # Stuff for executing recipes.
 
+def find_recipe(target):
+    for target_pattern in recipes:
+        recipe = recipes[target_pattern]
+        match, deps, command = recipe
+        pattern_parts = target_pattern.split(".")
+        if target == target_pattern:
+            print("1. {} == {}".format(target, target_pattern))
+            return recipe
+        elif len(pattern_parts) == 2 and pattern_parts[0] == "%" \
+                and pattern_parts[1] == target.split(".")[-1]:
+            print("2. {} ~= {}".format(target, target_pattern))
+            return recipe
+        else:
+            print("{} != {}".format(target, target_pattern))
+
+
 def run_recipe(target):
     if target is None:
         return
-    if not target in recipes:
+    recipe = find_recipe(target)
+    if recipe is None:
         print("No rule for target: {}".format(target), file=sys.stderr)
         exit(1)
-    match, deps, command = recipes[target]
-    print("{} => {}".format(target, deps))
-    print("      {}".format(command))
+    match, deps, command = recipe
+    print("FOUND: {} => {}".format(target, len(deps)))
+    print("")
     [run_recipe(dep) for dep in deps]
+
 
 _default = None
 def default(name):
@@ -56,6 +78,7 @@ def default(name):
     _default = name
     print("New default: {}".format(_default))
     return _default
+
 
 def build(argv=None):
     argv = argv or sys.argv
