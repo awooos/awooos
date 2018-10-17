8a4bee52dddf4f1cc0a39590cee998a9b430ee8f
Snuggly fops <kellerfuchs@hashbang.sh>
2018-10-17 15:51:07 -0400

    strtoll: Add tests with non-null endptr.

--- src/string_to_integer_test.c
+++ src/string_to_integer_test.c
@@ -24,10 +24,13 @@ size_t test_ali_str_to_int_assertions(void)
     TEST_ASSERT(strtol("-ABCD", NULL, 16) == -0xABCD);
     TEST_ASSERT(strtoll("-12a3bc89f", NULL, 16) == -0x12a3bc89f);
 
-    TEST_ASSERT(strtoll("-12a9 NaN 4321", NULL, 16) == -0x12a9);
-    TEST_ASSERT(strtoll("-12a9 NaN 4321", NULL, 10) == -12);
-
-    // TODO: Test strtol() with a non-null endptr.
+    const char *str = "-12a9 NaN 4321";
+    char *end10;
+    char *end16;
+    TEST_ASSERT(strtoll("-12a9 NaN 4321", &end16, 16) == -0x12a9);
+    TEST_ASSERT(strtoll("-12a9 NaN 4321", &end10, 10) == -12);
+    TEST_ASSERT(end16 == str + 5);
+    TEST_ASSERT(end10 == str + 3);
 
     TEST_ASSERTIONS_RETURN();
 }
