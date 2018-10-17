11ec8c9bc5d4e39e85adcea0889ef189d031ece2
The Fops in the Shell <kellerfuchs@hashbang.sh>
2018-10-17 15:19:49 -0400

    add strtol{,l} and ato{i,l,ll}

--- include/stdlib.h
+++ include/stdlib.h
@@ -8,4 +8,11 @@
 #define calloc(nmemb, size) ali_calloc(nmemb, size, __FUNCTION__, __FILE__, __LINE__)
 #define realloc(ptr, size) ali_realloc(ptr, size, __FUNCTION__, __FILE__, __LINE__)
 
+int atoi(const char *nptr);
+long atol(const char *nptr);
+long long atoll(const char *nptr);
+
+long int strtol(const char *nptr, char **endptr, int base);
+long long int strtoll(const char *nptr, char **endptr, int base);
+
 #endif
---
+++ src/string_to_integer.c
@@ -0,0 +1,54 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+static int digit(char c) {
+    if ('0' <= c && c <= '9')
+        return (c - '0');
+
+    else if ('a' <= c && c <= 'z')
+        return ((c - 'a') + 10);
+
+    else if ('A' <= c && c <= 'Z')
+        return ((c - 'A') + 10);
+
+    else return INT_MAX;
+}
+
+static bool is_digit(char c, int base) {
+    return digit(c) < base;
+}
+
+long long int strtoll(const char *nptr, char **endptr, int base)
+{
+    long long int ret = 0;
+
+    for(; is_digit(*nptr, base); nptr++)
+        ret = base * ret + digit(*nptr);
+
+    if(endptr != NULL)
+        *endptr = (char*)nptr;
+
+    return ret;
+}
+
+long int strtol(const char *nptr, char **endptr, int base)
+{
+    return (long int)strtoll(nptr, endptr, base);
+}
+
+int atoi(const char *nptr)
+{
+    return (int)strtol(nptr, NULL, 10);
+}
+
+long atol(const char *nptr)
+{
+    return (long)strtoll(nptr, NULL, 10);
+}
+
+long long atoll(const char *nptr)
+{
+    return strtoll(nptr, NULL, 10);
+}
+
