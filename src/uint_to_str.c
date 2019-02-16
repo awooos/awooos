55356bcff959bb86e9d44e32f78153c00ffe5f4c
Ellen Marie Dash <me@duckie.co>
2019-02-16 15:32:59 -0500

    skip leading zeroes in flail_uint_to_str()

--- src/uint_to_str.c
+++ src/uint_to_str.c
@@ -19,5 +19,13 @@ char *flail_uint_to_str(char buffer[UINT64_BUFSIZE],
         n /= radix;
     }
 
+    for (size_t idx = 0; idx < TINKER_UINT64_BUFSIZE; idx++) {
+        // If the first char is _not_ zero, or the second char is NULL, stop.
+        if (buffer[0] != '0' || buffer[1] == '\0') {
+            break;
+        }
+        buffer++;
+    }
+
     return buffer;
 }
