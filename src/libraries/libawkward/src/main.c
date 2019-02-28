6c5cffa927909686f97901dc8844d74b70f67d45
Ellen Marie Dash <me@duckie.co>
2019-02-28 16:17:12 -0500

    add faster 64-bit division-with-modulo

--- src/libraries/libawkward/src/main.c
+++ src/libraries/libawkward/src/main.c
@@ -1,23 +1,34 @@
 #include <stdint.h>
 
-/*
+// 64-bit division with modulo.
+uint64_t __udivmoddi4(uint64_t n, uint64_t d, uint64_t *rem)
+{
+    // TODO: What is the _proper_ way to handle this?
+    if (d == 0) { return 1 / d; }
+
+    uint64_t q = 0;
+    uint64_t tmp = n;
+
+    for (uint64_t idx = 0; idx < 64; idx++) {
+        if (d > tmp) {
+            continue;
+        }
+
+        tmp -= d;
+        q++;
+        d = d << 1;
+    }
+
+    *rem = tmp;
+
+    return q;
+}
+
 // 64-bit division for non-64-bit systems.
 uint64_t __udivdi3(uint64_t a, uint64_t b)
 {
-    uint64_t tmp = 0;
-
-    if (a < b) {
-        return 0;
-    }
-
-    while (a >= b) {
-        a -= b;
-        tmp += 1;
-    }
-
-    return tmp;
+    return __udivmoddi4(a, b, 0);
 }
-*/
 
 // 64-bit multiplication with overflow detection.
 uint64_t __mulodi4(uint64_t a, uint64_t b, int *overflow)
--- src/libraries/libawkward/src/main_test.c
+++ src/libraries/libawkward/src/main_test.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include "main.h"
 
-size_t test_awkward_assertions()
+size_t test_awkward_multiplication_assertions()
 {
     TEST_HAS_ASSERTIONS();
 
@@ -27,7 +27,26 @@ size_t test_awkward_assertions()
     TEST_ASSERTIONS_RETURN();
 }
 
+size_t test_awkward_division_assertions()
+{
+    TEST_HAS_ASSERTIONS();
+
+    int remainder = 0;
+    uint64_t ret;
+
+    ret = __mulodi4(13, 2, &remainder);
+    TEST_ASSERT(ret == 6);
+    TEST_ASSERT(remainder == 1);
+
+    ret = __mulodi4(1234567890123456789, 5, &remainder);
+    TEST_ASSERT(ret == 246913578024691357);
+    TEST_ASSERT(remainder == 4);
+
+    TEST_ASSERTIONS_RETURN();
+}
+
 void add_awkward_tests()
 {
-    tinker_add_test(awkward_assertions);
+    tinker_add_test(awkward_multiplication_assertions);
+    tinker_add_test(awkward_division_assertions);
 }
