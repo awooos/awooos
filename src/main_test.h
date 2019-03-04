b69ce9e8b37d549b4fbf8957ff60299fd0c88041
Ellen Marie Dash <me@duckie.co>
2019-03-03 22:21:53 -0500

    Revert back to 1MB universe chunk sizes.

--- src/main_test.h
+++ src/main_test.h
@@ -5,9 +5,7 @@
 #include <dmm.h>
 #include "main.h"
 
-// FIXME: Anything >634KB breaks tests.
-#define ALLOCATE_THE_UNIVERSE_CHUNK_SIZE (634 * 1024) // 634KB.
-//#define ALLOCATE_THE_UNIVERSE_CHUNK_SIZE (1024 * 1024) // 1M
+#define ALLOCATE_THE_UNIVERSE_CHUNK_SIZE (1024 * 1024) // 1M
 
 size_t test_dmm_malloc(void);
 size_t test_dmm_free_sets_header(void);
