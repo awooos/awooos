e1a10937b05c78d594a84af2bf8f543bd76ac158
Ellen Marie Dash <me@duckie.co>
2020-03-02 23:57:02 -0500

    Add a tinker_uint_to_str() test for the largest `unsigned long` value.

--- test/test_main.c
+++ test/test_main.c
@@ -10,6 +10,10 @@ void test_tinker_uint_to_str(void)
     tinker_assert(strcmp(tinker_uint_to_str(2), "2") == 0);
     tinker_assert(strcmp(tinker_uint_to_str(123), "123") == 0);
     tinker_assert(strcmp(tinker_uint_to_str(9001), "9001") == 0);
+
+    // tinker_uint_to_str() only supports `unsigned long`.
+    // This tests the longest number that can fit in an `unsigned long`.
+    tinker_assert(strcmp(tinker_uint_to_str(4294967295), "4294967295") == 0);
 }
 
 void add_tinker_main_tests(void)
