9295b2bc6c11114127b7dc0b8c414317a17eddae
Ellen Marie Dash <me@duckie.co>
2020-07-27 21:44:56 -0400

    Clean up math.c

--- src/math.c
+++ src/math.c
@@ -2,7 +2,7 @@
 
 int abs(int n) {
     if (n < 0) {
-        return n * -1;
+        return -n;
     } else {
         return n;
     }
@@ -10,7 +10,7 @@ int abs(int n) {
 
 double fabs(double n) {
     if (n < 0) {
-        return n * -1.0;
+        return -n;
     } else {
         return n;
     }
@@ -18,18 +18,16 @@ double fabs(double n) {
 
 double sqrt(double n)
 {
-    double guess = 10.0, newn;
-    int i;
-
-    /* This algorithm works as follows:
+    /* This is using the "Babylonian method", which works as follows:
      * 1. Make a guess and divide the number to be squared by it.
      * 2. Average the result of number 1 and the guess. This becomes the next guess.
-     * 3. Repeat.
-     * 4. There is no step 4.
+     * 3. Repeat step 2 until you reach the desired precision.
      */
 
-    for (i = 0; i < 100; i++) {
-        newn = n/guess;
+    double guess = 10.0;
+
+    for (int i = 0; i < 100; i++) {
+        double newn = n/guess;
         guess = (newn + n)/2.0;
         n = newn;
     }
