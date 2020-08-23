6608ca582f02eb450c7f4258aaab9a49d14e14dd
Ellen Marie Dash <me@duckie.co>
2020-08-23 12:45:45 -0400

    Test flail_stack_dump_hex(), too.

--- test/main.c
+++ test/main.c
@@ -1,10 +1,19 @@
 #include "../include/flail.h"
+#include "../src/main.h"
 #include <stddef.h>
 #include <stdio.h>
 
 
 void flail_stack_dump(void) {
-    puts("<insert stack dump here>");
+    size_t stack[] = {
+        0x1,                // Smallest valid/non-NULL value.
+        0x2,                // Arbitrary.
+        0xbadf00d,          // Arbitrary.
+        0xdeadbeef,         // Arbitrary.
+        0x9999999999999999, // Biggest integer size_t can fit.
+        0x0,                // Terminating NULL.
+    };
+    flail_stack_dump_hex(stack);
 }
 
 void flail_wait_forever(void) {
--- test/test_flail.py
+++ test/test_flail.py
@@ -12,18 +12,23 @@ def test_flail():
     path = Path(__file__).resolve().parent / 'flail-test'
     result = subprocess.run(str(path), capture_output=True, text=True, check=True)
 
-    assert result.stdout == dedent("""\
-        !!! Kernel panic !!!
+    lines = result.stdout.splitlines()
 
-        Flail test executable
-
-        This kernel panic is intentional.
-
-        Stack dump:
-
-        <insert stack dump here>
-
-
-        Kernel panic: This kernel panic is intentional.
-         src/main_test.c:17 (test_flail_intentional_panic)
-    """)
+    assert lines.pop(0) == "!!! Kernel panic !!!"
+    assert lines.pop(0) == ""
+    assert lines.pop(0) == "Flail test executable"
+    assert lines.pop(0) == ""
+    assert lines.pop(0) == "This kernel panic is intentional."
+    assert lines.pop(0) == ""
+    assert lines.pop(0) == "Stack dump:"
+    assert lines.pop(0) == ""
+    assert lines.pop(0).endswith(": 0x1")
+    assert lines.pop(0).endswith(": 0x2")
+    assert lines.pop(0).endswith(": 0xbadf00d")
+    assert lines.pop(0).endswith(": 0xdeadbeef")
+    assert lines.pop(0).endswith(": 0x9999999999999999")
+    assert lines.pop(0).endswith(": 0x0")
+    assert lines.pop(0) == ""
+    assert lines.pop(0) == ""
+    assert lines.pop(0) == "Kernel panic: This kernel panic is intentional."
+    assert lines.pop(0) == " src/main_test.c:17 (test_flail_intentional_panic)"
