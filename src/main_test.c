0bfc873f5f5065b03cbe206533a8d23c89d1c577
Ellen Marie Dash <me@duckie.co>
2021-03-28 21:35:28 -0400

    Add stdarg.h (with tests).

--- src/main_test.c
+++ src/main_test.c
@@ -4,6 +4,7 @@ void add_ali_tests()
 {
     add_ali_math_tests();
     add_ali_number_tests();
+    add_ali_stdarg_tests();
     add_ali_string_tests();
     add_ali_str_tests();
     add_ali_str_to_int_tests();
