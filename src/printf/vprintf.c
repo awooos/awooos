06e5bb1098e769b2115e15fba762d2c61693961d
Ellen Marie Dash <me@duckie.co>
2021-06-16 01:10:52 -0400

    Rename "fmt" to "format" in v*printf().

--- src/printf/vprintf.c
+++ src/printf/vprintf.c
@@ -10,11 +10,11 @@ int ali_vprint_arg(char *str, size_t size, const char *format, int *consumed, va
 //       xxputs is a basic wrapper around putchar which _does_ work there.
 //void xxputs(const char *str) { while(*str) { putchar(*str++); } }
 
-int vsnprintf(char *str, size_t size, const char *fmt, va_list args) {
+int vsnprintf(char *str, size_t size, const char *format, va_list args) {
     int length = 0;
     int tmp;
 
-    for (const char *p = fmt; *p; p++) {
+    for (const char *p = format; *p; p++) {
         // consumed can be modified in ali_vprint_arg() to adjust the
         // value of `p`, e.g. if there's a modifier (like "%.2f").
         int consumed = 0;
@@ -53,22 +53,22 @@ int vsnprintf(char *str, size_t size, const char *fmt, va_list args) {
     return length;
 }
 
-int vsprintf(char *str, const char *fmt, va_list args) {
+int vsprintf(char *str, const char *format, va_list args) {
     int len;
-    len = vsnprintf(NULL, 0, fmt, args);
-    len = vsnprintf(str, (size_t)(len + 1), fmt, args);
+    len = vsnprintf(NULL, 0, format, args);
+    len = vsnprintf(str, (size_t)(len + 1), format, args);
     return len;
 }
 
-int vprintf(const char *fmt, va_list args) {
+int vprintf(const char *format, va_list args) {
     char *str;
     int len;
 
-    len = vsnprintf(NULL, 0, fmt, args);
+    len = vsnprintf(NULL, 0, format, args);
     len++;
 
     str = malloc((size_t)len);
-    len = vsnprintf(str, (size_t)len, fmt, args);
+    len = vsnprintf(str, (size_t)len, format, args);
 
     for (size_t idx = 0; str[idx]; idx++) {
         putchar(str[idx++]);
