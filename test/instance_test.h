f2275840f237d5dc92bbd959a4c6a28e84c5786e
Ellen Marie Dash <me@duckie.co>
2023-10-02 14:40:00 -0400

    Remove commented-out code.

--- test/instance_test.h
+++ test/instance_test.h
@@ -4,8 +4,6 @@
 #include <stddef.h>
 #include <dmm.h>
 #include <dmm_instance.h>
-//#include "main.h"
-//#include "instance.h"
 
 void test_dmm_instance_add_region(void);
 void test_dmm_instance_malloc(void);
