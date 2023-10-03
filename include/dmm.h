5761944f5a62520fb0fa62a82cfedf397309a4db
Ellen Marie Dash <me@duckie.co>
2023-10-03 18:22:18 -0400

    Use __func__ instead of __FUNCTION__

--- include/dmm.h
+++ include/dmm.h
@@ -7,7 +7,7 @@ typedef void (DMM_PanicFn)(const char *message, const char *function,
         const char *filename, size_t line);
 
 extern DMM_PanicFn *dmm_pvt_panic;
-#define dmm_panic(message) dmm_pvt_panic(message, __FUNCTION__, __FILE__, __LINE__)
+#define dmm_panic(message) dmm_pvt_panic(message, __func__, __FILE__, __LINE__)
 
 void dmm_init(DMM_PanicFn *panic_fn);
 
@@ -15,15 +15,15 @@ void dmm_add_memory_region(void *start, size_t length);
 
 void *_dmm_malloc(size_t size, const char function[], const char filename[],
         size_t line);
-#define dmm_malloc(size) _dmm_malloc(size, __FUNCTION__, __FILE__, __LINE__)
+#define dmm_malloc(size) _dmm_malloc(size, __func__, __FILE__, __LINE__)
 
 void _dmm_free(void *ptr, const char function[], const char filename[],
         size_t line);
-#define dmm_free(ptr) _dmm_free(ptr, __FUNCTION__, __FILE__, __LINE__)
+#define dmm_free(ptr) _dmm_free(ptr, __func__, __FILE__, __LINE__)
 
 void *_dmm_realloc(void *ptr, size_t size, const char function[],
         const char filename[], size_t line);
-#define dmm_realloc(ptr, size) _dmm_realloc(ptr, size, __FUNCTION__, __FILE__, __LINE__)
+#define dmm_realloc(ptr, size) _dmm_realloc(ptr, size, __func__, __FILE__, __LINE__)
 
 // Tests
 void add_dmm_tests(void);
--- include/dmm_instance.h
+++ include/dmm_instance.h
@@ -7,15 +7,15 @@ void *dmm_instance_add_memory_region(void *instance, void *start, size_t length)
 
 void *_dmm_instance_malloc(void *instance, size_t size, const char function[],
         const char filename[], size_t line);
-#define dmm_instance_malloc(instance, size) _dmm_instance_malloc(instance, size, __FUNCTION__, __FILE__, __LINE__)
+#define dmm_instance_malloc(instance, size) _dmm_instance_malloc(instance, size, __func__, __FILE__, __LINE__)
 
 void _dmm_instance_free(void *instance, void *ptr, const char function[],
         const char filename[], size_t line);
-#define dmm_instance_free(instance, ptr) _dmm_instance_free(instance, ptr, __FUNCTION__, __FILE__, __LINE__)
+#define dmm_instance_free(instance, ptr) _dmm_instance_free(instance, ptr, __func__, __FILE__, __LINE__)
 
 void *_dmm_instance_realloc(void *instance, void *ptr, size_t size,
         const char function[], const char filename[], size_t line);
-#define dmm_instance_realloc(instance, ptr, size) _dmm_instance_realloc(instance, ptr, size, __FUNCTION__, __FILE__, __LINE__)
+#define dmm_instance_realloc(instance, ptr, size) _dmm_instance_realloc(instance, ptr, size, __func__, __FILE__, __LINE__)
 
 
 #endif
